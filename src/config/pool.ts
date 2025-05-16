bf03ebae03b0e001ddb69a197091a7bb483dc607
unknown <mututadaniel54@gmail.com>
2025-05-16 20:27:49 +0200

    changing api/products in routes to fatify

--- src/config/pool.ts
+++ src/config/pool.ts
@@ -1,3 +1,17 @@
+// import { Pool } from 'pg';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export default pool;
+
+
+
+
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
@@ -5,6 +19,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // Important for Railway managed Postgres
+  },
 });
 
 export default pool;
