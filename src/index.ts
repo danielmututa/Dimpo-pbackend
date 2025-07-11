abe8f78da407d3574852f6ecb462a5b22f4c7f9b
unknown <mututadaniel54@gmail.com>
2025-07-11 22:53:30 +0200

    adding last url for users  'http://localhost:3000'

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.register(cors, {
       'https://dimbop-users-site.vercel.app/',
       'https://dimbop-digital-marketing-dashboard.vercel.app',
       'http://localhost:5173',
-      'http://localhost:3000',
+       'http://localhost:3000'
     ];
 
     if (!origin) {
